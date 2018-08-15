206e805ce078c955bcb32efe35987b120537f2d0
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-15 15:05:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,22 @@ class Dog
   @name = name
   end
   
-  def name=()
+  def name
     @name = name
   end
   
+  def name=(fido)
+    @name = name
+  end
+end
+
+class Breed 
   def initialize(breed)
     @breed = breed
   end
-  
+  def breed
+    @breed = breed
+  end
   def breed=(breed)
     @breed = breed
   end
